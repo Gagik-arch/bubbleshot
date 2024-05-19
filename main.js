66ef2a666efb9b780b9058b4f62706b0105dcc0f
Gagik <75838391+Gagik-arch@users.noreply.github.com>
2024-05-19 16:00:55 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+import Canvas from "./src/canvas";
+
+const canvas = new Canvas();
+let id;
+
+window.addEventListener("load", (e) => {
+    const update = (timeStamp) => {
+        canvas.draw(timeStamp);
+        id = requestAnimationFrame(update);
+    };
+    update();
+});
+
+window.addEventListener("unload", () => {
+    cancelAnimationFrame(id);
+});
+
+window.addEventListener("resize", () => {
+    canvas.resize(window.innerWidth, window.innerHeight);
+    canvas.detectElementsCollision();
+});
+
+window.addEventListener("mousedown", (e) => {
+    canvas.onMouseDown(e);
+});
+
+window.addEventListener("mousemove", (e) => {
+    canvas.onMouseMove(e);
+});
+
+window.addEventListener("mouseup", (e) => {
+    canvas.onMouseUp(e);
+});
+
+window.addEventListener("contextmenu", (e) => {
+    e.preventDefault();
+});
+
+const startBtn = document.getElementById("start");
+const modal = document.getElementById("modal");
+
+startBtn.addEventListener("click", (e) => {
+    modal.style.display = "none";
+});
